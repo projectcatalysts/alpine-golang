948a9da66dd3f4d8b5adc043ed84e92b2d1ae1b4
Rik Kernaghan <rik.kernaghan@projectcatalysts.com>
2024-01-11 08:31:47 +1300

    Updated to latest CI scripts package & squashed commits

---
+++ build.sh
@@ -0,0 +1,94 @@
+#!/bin/bash -eu
+
+# build_alpine_golang builds the docker image that supports compiling golang files on alpine linux
+function build_alpine_golang {
+    local readonly no_cache_flag=${1}
+	local readonly package_name=${2}
+	local readonly package_push=${3}
+	local readonly package_version=${4}
+    local readonly package_is_latest=${5:-}
+
+    # Initialise docker passwords
+    procat_ci_docker_init
+
+    # Create the package downloads directory it if doesn't exist already
+    mkdir -p ${EXEC_CI_SCRIPT_PATH}/downloads/
+
+    # Remove any existing files from the container's download directory
+    rm -f ${EXEC_CI_SCRIPT_PATH}/downloads/*
+
+    # awk is necessary to remove the comment line.
+    #  e.g.  # Host gitlab.example.com found: line 52
+    local readonly sshkey_gitlab_server=$(ssh-keygen -H -F ${PROCAT_CI_GIT_SERVER} | awk 'NR==2')
+
+    # Copy files that can be utilised in the dockerfile to the container's download directory
+    echo ${sshkey_gitlab_server} > ${EXEC_CI_SCRIPT_PATH}/downloads/known_hosts
+
+    local readonly alpine_version="3.19"
+    local readonly golang_image="golang:${package_version}-alpine${alpine_version}"
+    local readonly base_image="${PROCAT_CI_REGISTRY_SERVER}/procat/docker/alpine-bash:latest"
+    local readonly build_args="--build-arg base_image=${base_image} --build-arg golang_image=${golang_image}"
+
+    # Build the docker image
+	procat_ci_docker_build_image ${no_cache_flag} ${package_name} ${package_push} ${package_version} ${package_is_latest} "${build_args}"
+}
+
+# configure_ci_environment is used to configure the CI environment variables
+function configure_ci_environment {
+    #
+    # Check the pre-requisite environment variables have been set
+    # PROCAT_CI_SCRIPTS_PATH would typically be set in .bashrc or .profile
+    # 
+    if [ -z ${PROCAT_CI_SCRIPTS_PATH+x} ]; then
+        echo "ERROR: A required CI environment variable has not been set : PROCAT_CI_SCRIPTS_PATH"
+        echo "       Has '~/.procat_ci_env.sh' been sourced into ~/.bashrc or ~/.profile?"
+        env | grep "PROCAT_CI"
+        return 1
+    fi
+
+    # Configure the build environment if it hasn't been configured already
+    source "${PROCAT_CI_SCRIPTS_PATH}/set_ci_env.sh"
+}
+
+function build {
+    #
+    # configure_ci_environment is used to configure the CI environment variables
+    # and load the CI common functions
+    #
+    configure_ci_environment || return $?
+
+    # For testing purposes, default the package name
+	if [ -z "${1-}" ]; then
+        local package_name=${PROCAT_CI_REGISTRY_SERVER}/procat/docker/alpine-golang
+        pc_log "package_name (default)           : $package_name"
+	else
+		local package_name=${1}
+        pc_log "package_name                     : $package_name"
+    fi
+
+    # For testing purposes, default the package version
+	if [ -z "${2-}" ]; then
+        local package_version="1.21.6"
+        pc_log "package_version (default)        : $package_version"
+	else
+		local package_version=${2}
+        pc_log "package_version                  : $package_version"
+    fi
+    pc_log ""
+
+	# Determine whether the --no-cache command line option has been specified.
+	# If it has, attempts to download files from the internet are always made.
+	if [ -z "${3-}" ]; then
+		local no_cache_flag="false"
+	else
+		local no_cache_flag=$([ "$3" == "--no-cache" ] && echo "true" || echo "false")
+	fi
+
+    # Build the docker image
+	build_alpine_golang ${no_cache_flag} ${package_name} push ${package_version} latest
+}
+
+# $1 : (Mandatory) Package Name (registry.projectcatalysts.prv/procat/docker/alpine-golang)
+# $2 : (Mandatory) Package Version (e.g. 3.14.2)
+# $3 : (Optional) --no-cache
+build ${1:-} ${2:-} ${3:-}
---
+++ dockerfile
@@ -0,0 +1,31 @@
+# Alpine Linux plus Golang
+ARG package_version
+ARG base_image
+ARG golang_image
+
+FROM ${golang_image} as golang
+FROM ${base_image}
+LABEL package_version="${package_version}"
+
+# copy required files to image
+# https://stackoverflow.com/questions/52056387/how-to-install-go-in-alpine-linux
+COPY --from=golang /usr/local/go /usr/local/go
+COPY ./downloads/known_hosts /root
+
+# environment
+ENV GOLANG_VERSION ${package_version}
+ENV GOROOT /usr/local/go
+ENV GOPATH /go
+ENV PATH ${GOPATH}/bin:${GOROOT}/bin:$PATH
+
+# do all in one step
+RUN apk --no-cache add openssh git zip \
+    && mkdir -p /src ~/.ssh/ ${GOPATH}/src ${GOPATH}/bin \
+    && cat /root/known_hosts > ~/.ssh/known_hosts \
+    && chmod -R 777 $GOPATH \
+    && chmod -R 777 /src \
+    # cleanup
+    && rm /root/known_hosts \
+    # final check
+    && go version
+WORKDIR /src
